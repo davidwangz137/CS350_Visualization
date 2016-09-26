a6f6567742c9dad5a574bf8192ce4ee93654f4e0
david <davidwangz137@github.com>
2016-09-26 12:26:54 -0400

    adding traffic A1P2 visualization

---
+++ traffic_state.py
@@ -0,0 +1,225 @@
+"""
+Contains the logic for handling the different game events and animating them
+"""
+
+import pygame
+clock = pygame.time.Clock()
+
+# TODO: draw a number on each square to denote how may are there in that queue
+
+# A list of the different directions
+directions = ['N', 'E', 'S', 'W']
+
+# Utility functions for finding the kind of turn it is
+def right_turn(origin, dest):
+    if origin+dest in ['NW', 'EN', 'SE', 'WS']:
+        return True
+    return False
+
+def left_turn(origin, dest):
+    if origin+dest in ['NE', 'ES', 'SW', 'WN']:
+        return True
+    return False
+
+def straight(origin, dest):
+    if origin+dest in ['NS', 'EW', 'SN', 'WE']:
+        return True
+    return False
+
+# Draws a square with the corresponding center
+# Rotate it according to the origin of the car
+# S: 0 degrees
+# E: 90 degrees
+# N: 180 degrees
+# W: 270 degrees
+
+side_length = 24
+def draw_square(center, origin, color, number, text):
+    center[0] -= window_side_length/2
+    center[1] -= window_side_length/2
+    if origin == 'N':
+        center = [-center[0], -center[1]]
+    elif origin == 'E':
+        center = [center[1], -center[0]]
+    elif origin == 'S':
+        center = [center[0], center[1]]
+    elif origin == 'W':
+        center = [-center[1], center[0]]
+    else:
+        raise Exception("Invalid origin: %s" % origin)
+    center[0] += window_side_length/2
+    center[1] += window_side_length/2
+    pygame.draw.rect(screen, color, pygame.Rect(center[0]-side_length/2, center[1]-side_length/2, side_length, side_length))
+    # Now draw the text
+    if text:
+        text = font.render("%d" % number, True, (255, 255, 255))
+        screen.blit(text, (center[0] - text.get_width()/2, center[1] - text.get_height()/2))
+
+# Spawn any now existing stacks
+# Despawn any stacks that don't exist anymore
+# Update the numbers on those that still do
+
+base = 460
+spacing = 50
+def process_tile(k, v, y, base=base, text=True):
+    if v == 0:
+        return
+    if right_turn(k[0], k[1]):
+        draw_square([base + 2*spacing, y], k[0], (0, 255, 0), v, text)
+    elif straight(k[0], k[1]):
+        draw_square([base + spacing, y], k[0], (0, 128, 255), v, text)
+    elif left_turn(k[0], k[1]):
+        draw_square([base, y], k[0], (255, 100, 0), v, text)
+    else:
+        raise Exception("Not a valid turn: %s %s" %(k[0], k[1]))
+
+wait_y = 670
+inter_y = 560
+
+# This simply updates the graphics
+def update_graphics(wait, inter):
+    # Clear the screen of all previous graphics
+    screen.fill((0, 0, 0))
+
+    for k, v in wait.iteritems():
+        process_tile(k, v, wait_y)
+
+    for k, v in inter.iteritems():
+        process_tile(k, v, inter_y)
+
+    # Draw the traffic boundaries
+    color = (255, 255, 255)
+    thickness = 20
+    pygame.draw.rect(screen, color, pygame.Rect(0, base + 3*spacing, window_side_length, thickness))
+    pygame.draw.rect(screen, color, pygame.Rect(0, window_side_length - (base + 3*spacing) - thickness, window_side_length, thickness))
+    pygame.draw.rect(screen, color, pygame.Rect(base + 3*spacing, 0, thickness, window_side_length))
+    pygame.draw.rect(screen, color, pygame.Rect(window_side_length - (base + 3*spacing) - thickness, 0, thickness, window_side_length))
+
+    # Traffic lines on the side for decorations
+    thickness = 10
+    length = window_side_length - (base + 3*spacing) - thickness
+    pygame.draw.rect(screen, color, pygame.Rect(0, window_side_length/2-thickness/2, length, thickness))
+    pygame.draw.rect(screen, color, pygame.Rect(base + 3*spacing + thickness, window_side_length/2-thickness/2, length, thickness))
+    pygame.draw.rect(screen, color, pygame.Rect(window_side_length/2-thickness/2, 0, thickness, length))
+    pygame.draw.rect(screen, color, pygame.Rect(window_side_length/2-thickness/2, base + 3*spacing + thickness, thickness, length))
+
+"""
+Animation code
+"""
+
+step = 5  # The animation step size in pixels
+# This animates from the wait queue to the inter queue
+def road_to_wait(wait, inter, origin, dest):
+    for i in range((window_side_length - wait_y)/step):
+        update_graphics(wait, inter)
+        process_tile((origin, dest), 1, window_side_length - i*step, base, False)
+        pygame.display.flip()
+        clock.tick(120)
+
+# This animates from the wait queue to the inter queue
+def wait_to_inter(wait, inter, origin, dest):
+    for i in range((wait_y-inter_y)/step):
+        update_graphics(wait, inter)
+        process_tile((origin, dest), 1, wait_y-i*step, base, False)
+        pygame.display.flip()
+        clock.tick(120)
+
+# This animates from the inter queue to the finish queue
+def inter_to_fin(wait, inter, origin, dest):
+
+    # Depending on the turn, do a different animation
+    if right_turn(origin, dest):
+        # Go to center of right lane
+        center_right = 50
+        for i in range(center_right/step):
+            update_graphics(wait, inter)
+            process_tile((origin, dest), 1, inter_y-i*step, base, False)
+            pygame.display.flip()
+            clock.tick(120)
+        # Go into right lane
+        for i in range((window_side_length-base-2*spacing)/step):
+            update_graphics(wait, inter)
+            process_tile((origin, dest), 1, inter_y-center_right, base+i*step, False)
+            pygame.display.flip()
+            clock.tick(120)
+
+    elif straight(origin, dest):
+        # Go straight
+        for i in range(inter_y/step):
+            update_graphics(wait, inter)
+            process_tile((origin, dest), 1, inter_y-i*step, base, False)
+            pygame.display.flip()
+            clock.tick(120)
+    elif left_turn(origin, dest):
+        # Go to center of left lane
+        center_left = 270
+        for i in range(center_left/step):
+            update_graphics(wait, inter)
+            process_tile((origin, dest), 1, inter_y-i*step, base, False)
+            pygame.display.flip()
+            clock.tick(120)
+        # Go into left lane
+        for i in range(base/step):
+            update_graphics(wait, inter)
+            process_tile((origin, dest), 1, inter_y-center_left, base-i*step, False)
+            pygame.display.flip()
+            clock.tick(120)
+    else:
+        raise Exception("Not a valid turn: %s %s" %(origin, dest))
+
+"""
+Traffic Logic
+"""
+
+# Maintain queues for each direction to each direction for whether they are waiting or in the intersection itself
+# Upon a change, also animate the change that occured
+class TrafficState:
+    def __init__(self):
+        self.wait = {}
+        self.inter = {}
+        for d in directions:
+            for d2 in directions:
+                # No cars U-turn
+                if d == d2:
+                    continue
+                self.wait[(d, d2)] = 0
+                self.inter[(d, d2)] = 0
+
+    def waiting(self, origin, dest, animate):
+
+        # Do the animation. This will call update graphics, so we don't have to worry about the rest of the squares
+        if animate:
+            road_to_wait(self.wait, self.inter, origin, dest)
+
+        # Need to update again for the number to show up
+        self.wait[(origin, dest)] += 1
+        update_graphics(self.wait, self.inter)
+
+    def in_intersection(self, origin, dest, animate):
+
+        # move a car to the intersection from the waiting section
+        self.wait[(origin, dest)] -= 1
+
+        # Do the animation. This will call update graphics, so we don't have to worry about the rest of the squares
+        if animate:
+            wait_to_inter(self.wait, self.inter, origin, dest)
+
+        # Need to update again for the number to show up
+        self.inter[(origin, dest)] += 1
+        update_graphics(self.wait, self.inter)
+
+    def leaving_intersection(self, origin, dest, animate):
+
+        # The car moves from the intersection
+        self.inter[(origin, dest)] -= 1
+
+        # Do the animation. This will call update graphics, so we don't have to worry about the rest of the squares
+        if animate:
+            inter_to_fin(self.wait, self.inter, origin, dest)
+
+
+# We initialize pygame here so we have access to the window parameters
+window_side_length = 800
+pygame.init()
+screen = pygame.display.set_mode((window_side_length, window_side_length))
+font = pygame.font.SysFont("comicsansms", 36)
---
+++ visualize_traffic.py
@@ -0,0 +1,99 @@
+"""
+Reads the simulation events from the command line passed in file and reruns the
+simulation with animations Basically, to use this you need to have the
+following print statements in your traffic simulation output log (where D is
+the direction in ['N', 'E', 'S', 'W']):
+
+PUT THIS SOMEWHERE IN THE SIMULATION INITIALIZATION
+>>>INITIALIZING_SIMULATION
+
+PUT THIS BEFORE ALL THE LOCKS BEFORE YOU ENTER THE INTERSECTION
+>>>WAIT_FOR_INTERSECTION: D D
+
+PUT THIS AFTER ALL THE LOCKS BEFORE YOU ENTER THE INTERSECTION
+>>>GOING_INTO_INTERSECTION: D D
+
+PUT THIS BEFORE YOU UNBLOCK A CAR WHEN YOU ARE LEAVING THE INTERSECTION OR ELSE
+THAT CAR WILL PRINT ENTERING FIRST BEFORE THIS PRINTS
+>>>LEAVING_INTERSECTION: D D
+
+NOTE: PUT THIS SOMEWHERE IN THE SIMULATION CLEANUP
+>>>FINISHING_SIMULATION
+"""
+
+import pygame
+import time
+import re
+import sys
+from traffic_state import TrafficState
+
+# Get the file events from the file passed in 
+if len(sys.argv) < 2:
+    print "No simulation log file passed in!"
+    sys.exit()
+
+# If a fast forward point in the events is passed in, then we only animate starting at that point (since we only want to see events after that)
+if len(sys.argv) == 3:
+    print "Fast forwarding to set breakpoint event!"
+    try:
+        fast_forward = int(sys.argv[2])
+    except Exception as e:
+        print "Error parsing fast forward point (should be an integer)"
+        raise e
+
+sim_file = file(sys.argv[1], "r")
+
+# Process the lines into events
+lines = sim_file.readlines()
+ind_start = 0
+
+# Read to the start of the simulation
+while True:
+    if re.match(r'>>>INITIALIZING_SIMULATION', lines[ind_start]):
+        break
+    ind_start += 1
+
+# Read to the end of the simulation
+ind_end = ind_start
+while True:
+    if re.match(r'>>>FINISHING_SIMULATION', lines[ind_end]):
+        break
+    ind_end += 1
+
+lines = lines[(ind_start+1):ind_end]  # We don't want to include the starting line
+
+# Process all of the prints into events
+events = []
+for line in lines:
+    event = re.match(r'>>>(.*): (\w) (\w)', line).groups()
+    assert len(event) == 3
+    events.append(event)
+
+"""
+Simulate the traffic again
+"""
+
+# Since we are making everything symmetrical, the window is a square and all the animations are simply rotations of each other depending on which direction they are
+
+# Create the traffic simulator
+traffic = TrafficState()
+animate = False
+
+for i, event in enumerate(events):
+    print "%d:" % i, event
+    # Check if we have hit our fast_forward point
+    if i == fast_forward:
+        animate = True
+
+    if event[0] == "WAIT_FOR_INTERSECTION":
+        traffic.waiting(event[1], event[2], animate)
+    elif event[0] == "GOING_INTO_INTERSECTION":
+        traffic.in_intersection(event[1], event[2], animate)
+    elif event[0] == "LEAVING_INTERSECTION":
+        traffic.leaving_intersection(event[1], event[2], animate)
+    else:
+        raise Exception("Unknown event: %s" % event[0])
+
+    if animate:
+        pygame.display.flip()
+        time.sleep(0.1)
